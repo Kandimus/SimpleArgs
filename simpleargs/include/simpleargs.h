daa6167c68188a82ca69ba5fab883b6aa1aa6ffa
Vitalii Litvinov <vitalii.litvinov@thetoadman.com>
2023-08-31 13:51:41 +0200

    fixed a issue when a argument value was written without equal sign

--- simpleargs/include/simpleargs.h
+++ simpleargs/include/simpleargs.h
@@ -2,25 +2,21 @@
 //===
 //=== simpleargs.h
 //===
-//=== Copyright (c) 2020 by RangeSoft.
+//=== Copyright (c) 2020-2023 by RangeSoft.
 //=== All rights reserved.
 //===
-//=== Litvinov "VeduN" Vitaliy O.
+//=== Litvinov "VeduN" Vitaly
 //===
 //=================================================================================================
 //===
 //=== Класс для обработки параметров командной строки
 //===
 //=================================================================================================
-
 #pragma once
 
 #include <vector>
 #include <string>
 
-
-//-------------------------------------------------------------------------------------------------
-//
 class rSimpleArgs
 {
 private:
@@ -34,30 +30,140 @@ private:
 	};
 
 public:
-	virtual ~rSimpleArgs();
+    virtual ~rSimpleArgs() = default;
 	static rSimpleArgs& instance() { static rSimpleArgs Singleton; return Singleton; }
 private:
-	rSimpleArgs();
-	rSimpleArgs(const rSimpleArgs& );
-	rSimpleArgs& operator=(rSimpleArgs& );
+    rSimpleArgs() = default;
+    rSimpleArgs(const rSimpleArgs&);
+    rSimpleArgs& operator=(rSimpleArgs&);
 
 public:
-	rSimpleArgs& addSwitch(const std::string& name, const unsigned char altname);
-	rSimpleArgs& addOption(const std::string& name, const unsigned char altname, const std::string& default_value);
-	rSimpleArgs& setSwitch(const std::string& name, bool isset);
-	rSimpleArgs& setOption(const std::string& name, const std::string& value);
-	unsigned int isSet(const std::string& name);
-	std::string  getOption(const std::string& name);
-	std::string  getArgument(unsigned int num);
-	unsigned int getCountArgument(void);
+    rSimpleArgs& addSwitch(const std::string& name, const unsigned char shortname)
+    {
+        auto item = findItem(name);
+
+        if (item == nullptr)
+        {
+            m_list.push_back(rSimpleArgs::rItem());
+            item = &m_list.back();
+        }
+
+        item->m_isSwitch  = true;
+        item->m_isSet     = 0;
+        item->m_fullname  = name;
+        item->m_shortname = shortname;
+        item->m_value     = "";
+
+        return *this;
+    }
+    rSimpleArgs& addOption(const std::string& name, const unsigned char shortname, const std::string& default_value)
+    {
+        rItem* item = findItem(name);
+
+        if (item == nullptr)
+        {
+            m_list.push_back(rSimpleArgs::rItem());
+            item = &m_list.back();
+        }
+
+        item->m_isSwitch  = false;
+        item->m_isSet     = 0;
+        item->m_fullname  = name;
+        item->m_shortname = shortname;
+        item->m_value     = default_value;
+
+        return *this;
+    }
+    rSimpleArgs& setSwitch(const std::string& name, bool isset)
+    {
+        rItem* item = findItem(name);
+
+        if (item == nullptr)
+        {
+            m_list.push_back(rSimpleArgs::rItem());
+            item = &m_list.back();
+            item->m_fullname = name;
+        }
+
+        item->m_isSet = isset;
+        return *this;
+    }
+    rSimpleArgs& setOption(const std::string& name, const std::string& value)
+    {
+        rItem* item = findItem(name);
+
+        if (item == nullptr)
+        {
+            m_list.push_back(rSimpleArgs::rItem());
+            item = &m_list.back();
+
+            item->m_fullname = name;
+        }
+
+        item->m_value = value;
+        item->m_isSet = true;
+        return *this;
+    }
+    unsigned int isSet(const std::string& name) const
+    {
+        auto item = findConstItem(name);
+        return item ? item->m_isSet : false;
+    }
+    std::string  getOption(const std::string& name) const
+    {
+        auto item = findConstItem(name);
+        return item ? item->m_value : "";
+    }
+    std::string  getArgument(unsigned int num) const
+    {
+        return num < m_argument.size() ? m_argument[num] : "";
+    }
+    unsigned int getCountArgument() const
+    {
+        return m_argument.size();
+    }
+
 	unsigned int parse(unsigned int argc, const char **argv);
 
 private:
 	std::vector<rSimpleArgs::rItem> m_list;
 	std::vector<std::string> m_argument;
 
-	rItem* findItem(const std::string& name);
-	void parseShort(const std::string& arg);
+    const rItem* findConstItem(const std::string& name) const
+    {
+        for (auto& item : m_list)
+        {
+            if (item.m_fullname == name)
+            {
+                return &item;
+            }
+        }
+        return nullptr;
+    }
+
+    rItem* findItem(const std::string& name)
+    {
+        for (auto& item : m_list)
+        {
+            if (item.m_fullname == name)
+            {
+                return &item;
+            }
+        }
+        return nullptr;
+    }
+
+    void parseShort(const std::string& arg)
+    {
+        std::string str = arg.substr(1);
+        for (auto& item : m_list) {
+            int pos = str.find(item.m_shortname);
+            if(pos >= 0) {
+                ++item.m_isSet;
+            }
+        }
+    }
+
 	rItem* parseFull(const std::string& arg);
 };
 
--- simpleargs/src/simpleargs.cpp
+++ simpleargs/src/simpleargs.cpp
@@ -15,121 +15,6 @@
 
 #include "simpleargs.h"
 
-rSimpleArgs::rSimpleArgs()
-{
-
-}
-
-rSimpleArgs::~rSimpleArgs()
-{
-
-}
-
-rSimpleArgs::rItem* rSimpleArgs::findItem(const std::string& name)
-{
-	for(auto& item : m_list) {
-		if (item.m_fullname == name) {
-			return &item;
-		}
-	}
-
-	return nullptr;
-}
-
-rSimpleArgs& rSimpleArgs::addSwitch(const std::string& name, const unsigned char shortname)
-{
-	rItem* item = findItem(name);
-
-	if (item == nullptr) {
-		m_list.push_back(rSimpleArgs::rItem());
-		item = &m_list.back();
-	}
-
-	item->m_isSwitch  = true;
-	item->m_isSet     = 0;
-	item->m_fullname  = name;
-	item->m_shortname = shortname;
-	item->m_value     = "";
-
-	return *this;
-}
-
-rSimpleArgs& rSimpleArgs::addOption(const std::string& name, const unsigned char shortname, const std::string&  default_value)
-{
-	rItem* item = findItem(name);
-
-	if (item == nullptr) {
-		m_list.push_back(rSimpleArgs::rItem());
-		item = &m_list.back();
-	}
-
-	item->m_isSwitch  = false;
-	item->m_isSet     = 0;
-	item->m_fullname  = name;
-	item->m_shortname = shortname;
-	item->m_value     = default_value;
-
-	return *this;
-}
-
-rSimpleArgs& rSimpleArgs::setSwitch(const std::string& name, bool isset)
-{
-	rItem* item = findItem(name);
-
-	if (item == nullptr) {
-		m_list.push_back(rSimpleArgs::rItem());
-		item = &m_list.back();
-
-		item->m_fullname = name;
-	}
-
-	item->m_isSet = isset;
-
-	return *this;
-}
-
-rSimpleArgs& rSimpleArgs::setOption(const std::string& name, const std::string& value)
-{
-	rItem* item = findItem(name);
-
-	if (item == nullptr) {
-		m_list.push_back(rSimpleArgs::rItem());
-		item = &m_list.back();
-
-		item->m_fullname = name;
-	}
-
-	item->m_value = value;
-	item->m_isSet = true;
-
-	return *this;
-}
-
-
-unsigned int rSimpleArgs::isSet(const std::string& name)
-{
-	const rItem* item = findItem(name);
-	return item ? item->m_isSet : false;
-}
-
-std::string rSimpleArgs::getOption(const std::string& name)
-{
-	const rItem* item = findItem(name);
-	return item ? item->m_value : "";
-}
-
-std::string rSimpleArgs::getArgument(unsigned int num)
-{
-	return num < m_argument.size() ? m_argument[num] : "";
-}
-
-unsigned int rSimpleArgs::getCountArgument(void)
-{
-	return m_argument.size();
-}
-
-//-------------------------------------------------------------------------------------------------
-//
 unsigned int rSimpleArgs::parse(unsigned int argc, const char **argv)
 {
 	rItem* curarg = nullptr;
@@ -167,7 +52,9 @@ unsigned int rSimpleArgs::parse(unsigned int argc, const char **argv)
 
 		if (curarg) {
 			curarg->m_value = arg;
+            ++curarg->m_isSet;
 			curarg = nullptr;
+
 			continue;
 		} else {
 			m_argument.push_back(arg);
@@ -177,33 +64,22 @@ unsigned int rSimpleArgs::parse(unsigned int argc, const char **argv)
 	return 0;
 }
 
-
-void rSimpleArgs::parseShort(const std::string& arg)
+rSimpleArgs::rItem* rSimpleArgs::parseFull(const std::string& arg/*, bool& valueSet*/)
 {
-	std::string str = arg.substr(1);
-	for (auto& item : m_list) {
-		int pos = str.find(item.m_shortname);
-		if(pos >= 0) {
-			++item.m_isSet;
-		}
-	}
-}
-
-rSimpleArgs::rItem* rSimpleArgs::parseFull(const std::string& arg)
-{
-	std::string name  = arg.substr(2);
+    std::string name = arg.substr(2);
 	std::string value = "";
-
 	int pos = name.find('=');
+
+    //valueSet = false;
 	if (pos >= 0) {
 		value = name.substr(pos + 1);
 		name  = name.substr(0, pos);
+        //valueSet = true;
 	}
 
 	for (auto& item : m_list) {
 		if (item.m_fullname == name) {
-			++item.m_isSet;
-
+            item.m_isSet += pos >= 0;
 			if (value.size()) {
 				item.m_value = value;
 			}
